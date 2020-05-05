0fe50582c6ba916b78e8d6f9d4781e757f4c7122
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2020-05-06 01:20:33 +0200

    Add other inputs & fix issues

--- app.js
+++ app.js
@@ -1,66 +1,152 @@
-document.getElementById("name").addEventListener("blur", validateName);
-document.getElementById("email").addEventListener("blur", validateEmail);
-document.getElementById("phone").addEventListener("blur", validatePhone);
-document.getElementById("zip").addEventListener("blur", validateZip);
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+const formValidInput = {
+  name: false,
+  username: false,
+  email: false,
+  phone: false,
+  password: false,
+  password2: false,
+  allValid() {
+    return (
+      this.name &&
+      this.username &&
+      this.email &&
+      this.phone &&
+      this.password &&
+      this.password2
+    );
+  },
+};
 
-document
-  .querySelector(".form-validate")
-  .addEventListener("submit", checkValidate);
-let allValid = false;
+// /////////////////////////////////////////////////
+// Functions
+// /////////////////////////////////////////////////
+// ? Show Error function
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.classList.add("error");
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+  formValidInput[input.id] = false;
+}
 
-function validateName() {
-  const name = document.getElementById("name");
-  const re = /^[a-z\s]{3,16}$/i;
+// ? Show success function
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.classList.remove("error");
+  formControl.classList.add("success");
+  formValidInput[input.id] = true;
+}
 
-  if (!re.test(name.value)) {
-    name.classList.add("is-invalid");
-    allValid = false;
+// ?Check Name
+function isValidName() {
+  const re = /^[a-z(\s)?]{3,18}$/i;
+  if (name.value.trim() === "") {
+    showError(name, "Name is required.");
+  } else if (!re.test(name.value)) {
+    showError(name, "Name must be between 3 and 18 characters.");
   } else {
-    name.classList.remove("is-invalid");
-    allValid = true;
+    showSuccess(name);
   }
 }
 
-function validateEmail() {
-  const email = document.getElementById("email");
-  const re = /^([a-z0-9_\.\-])+(@)[a-z]+\.([a-z]{2,5})$/i;
-
-  if (!re.test(email.value)) {
-    email.classList.add("is-invalid");
-    allValid = false;
+// ? check username
+function isValidUsername() {
+  const re = /^[a-z]\w+$/i;
+  if (username.value.trim() === "") {
+    showError(username, "Username is required.");
+  } else if (!re.test(username.value)) {
+    showError(username, "Enter valid username.");
   } else {
-    email.classList.remove("is-invalid");
-    allValid = true;
+    showSuccess(username);
   }
 }
 
-function validatePhone() {
-  const phone = document.getElementById("phone");
+// ? Check Email
+function isValidEmail() {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/;
+  if (email.value.trim() === "") {
+    showError(email, "Email is required.");
+  } else if (!re.test(email.value)) {
+    showError(email, "Enter valid email.");
+  } else {
+    showSuccess(email);
+  }
+}
+
+// ? Check Phone Number
+function isValidPhoneNumber() {
   const re = /^(\+2)?\d{11}$/;
-  if (!re.test(phone.value)) {
-    phone.classList.add("is-invalid");
-    allValid = false;
+  if (phone.value.trim() === "") {
+    showError(phone, "Phone number is required.");
+  } else if (!re.test(phone.value)) {
+    showError(phone, "Enter valid phone number, [+20-1x-xxxx-xxxx].");
   } else {
-    phone.classList.remove("is-invalid");
-    allValid = true;
+    showSuccess(phone);
   }
 }
 
-function validateZip() {
-  const zip = document.getElementById("zip");
-  const re = /^\d{5}$/;
-
-  if (!re.test(zip.value)) {
-    zip.classList.add("is-invalid");
-    allValid = false;
+// ? Check Password
+function isValidPassword() {
+  const re = /.{8,}/;
+  if (password.value === "") {
+    showError(password, "Password is required.");
+  } else if (!re.test(password.value)) {
+    showError(password, "Password must be at least 8 characters.");
   } else {
-    zip.classList.remove("is-invalid");
-    allValid = true;
+    showSuccess(password);
   }
 }
 
-function checkValidate(e) {
-  if (!allValid) {
+// ? Check Password match
+function isValidPasswordMatch() {
+  if (password.value === "") {
+    showError(password2, "Confirm password is required.");
+  } else if (password.value !== password2.value) {
+    showError(password2, "Password do not match");
+  } else {
+    showSuccess(password2);
+  }
+}
+
+function callAll() {
+  isValidName();
+  isValidUsername();
+  isValidEmail();
+  isValidPhoneNumber();
+  isValidPassword();
+  isValidPasswordMatch();
+}
+// /////////////////////////////////////////////////
+// Event listeners
+// /////////////////////////////////////////////////
+form.addEventListener("submit", (e) => {
+  // Check input
+  callAll();
+  if (!formValidInput["allValid"]()) {
     e.preventDefault();
+    console.log("error");
+  } else {
+    // console.log("passed");
   }
-}
+});
+
+name.addEventListener("blur", isValidName);
+
+username.addEventListener("blur", isValidUsername);
+
+email.addEventListener("blur", isValidEmail);
+
+phone.addEventListener("blur", isValidPhoneNumber);
+
+password.addEventListener("blur", isValidPassword);
+
+password2.addEventListener("blur", isValidPasswordMatch);
+
+// //////////////////////////////
